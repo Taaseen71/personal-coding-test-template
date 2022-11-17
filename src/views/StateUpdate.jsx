46807bb3c643f0511e19b3b0c9f6e055ed378cc9
taaseen71 <dev.saadat@gmail.com>
2022-11-17 00:26:31 -0500

    added solutions

--- src/views/StateUpdate.jsx
+++ src/views/StateUpdate.jsx
@@ -21,42 +21,18 @@ const StateUpdate = () => {
             setInformation(data)
         }
 
-        const handleTitleChange = (id1, e) => {
+          const handleTitleChange = (id, e) => {
+            setInformation(data => {
+                data.experience[id].title = e.target.value;
+                return {...data}
+            })
         }
         const handleResponsibilityChange = (id, id2, e) => {
+            setInformation(data => {
+                data.experience[id].responsibilities[id2] = e.target.value;
+                return {...data}
+            })
         }
-
-        {information.experience && information.experience.map((job, id) => {
-                    return(
-                        <div key={id}>
-                            <div>
-                                <h2>Title</h2>
-                                <div>{job.title}</div>
-                                <textarea 
-                                    name="job-title" 
-                                    cols="30" 
-                                    rows="1" 
-                                    value={job.title} 
-                                    onChange={(e) => {handleTitleChange(id,e)}}
-                                ></textarea>
-                            </div>
-                            <div>
-                                <h3>Responsibilities:</h3>
-                                {job.responsibilities.map((responsibility,id2) => (
-                                    <div  key={id2}>
-                                        <div>{responsibility}</div>
-                                        <textarea 
-                                            name="job-title" cols="50" 
-                                            rows="1"
-                                            value={responsibility} 
-                                            onChange={(e) => {handleResponsibilityChange(id, id2, e)}}
-                                        ></textarea>
-                                    </div>
-                                ))}
-                            </div>
-                        </div>
-                    )
-        })}
 
     return (
         <div>
@@ -67,33 +43,69 @@ const StateUpdate = () => {
                 <div>
                     <h2>Your Task1 here is to use the "Object.map" function to display all values for jobs and all values of responsibilities.</h2>
                     <h2>Task2 is to use the given Text-area to update the state of the values using the onChange() function. The functions are provided for your convenicence.</h2>
-                    {information.experience && 
-                        <div>
-                            <div>
-                                <h3>Title</h3>
-                                <div>{information.experience[0].title}</div>
-                                <textarea 
-                                name="job-title" 
-                                cols="30" 
-                                rows="1" 
-                                value={information.experience[0].title} 
-                                onChange={(e) => {}}
-                                ></textarea>
-                            </div>
-                            <div>
-                                <h3>Responsibilities:</h3>
+                    {information.experience && information.experience.map((job, id) => {
+                        return(
+                            <div key={id}>
                                 <div>
-                                    <div>{information.experience[0].responsibilities[0]}</div>
+                                    <h2>Title</h2>
+                                    <div>{job.title}</div>
                                     <textarea 
-                                        name="job-title" cols="50" 
-                                        rows="1"
-                                        value={information.experience[0].responsibilities[0]} 
-                                        onChange={(e) => {}}
+                                        name="job-title" 
+                                        cols="30" 
+                                        rows="1" 
+                                        value={job.title} 
+                                        onChange={(e) => {handleTitleChange(id,e)}}
                                     ></textarea>
                                 </div>
+                                <div>
+                                    <h3>Responsibilities:</h3>
+                                    {job.responsibilities.map((responsibility,id2) => (
+                                        <div  key={id2}>
+                                            <div>{responsibility}</div>
+                                            <textarea 
+                                                name="job-title" cols="50" 
+                                                rows="1"
+                                                value={responsibility} 
+                                                onChange={(e) => {handleResponsibilityChange(id, id2, e)}}
+                                            ></textarea>
+                                        </div>
+                                    ))}
+                                </div>
                             </div>
-                        </div>
-                    }
+                        )
+                    })}
+
+                    {information.experience && information.experience.map((job, id) => {
+                        return(
+                            <div key={id}>
+                                <div>
+                                    <h2>Title</h2>
+                                    <div>{job.title}</div>
+                                    <textarea 
+                                        name="job-title" 
+                                        cols="30" 
+                                        rows="1" 
+                                        value={job.title} 
+                                        onChange={(e) => {handleTitleChange(id,e)}}
+                                    ></textarea>
+                                </div>
+                                <div>
+                                    <h3>Responsibilities:</h3>
+                                    {job.responsibilities.map((responsibility,id2) => (
+                                        <div  key={id2}>
+                                            <div>{responsibility}</div>
+                                            <textarea 
+                                                name="job-title" cols="50" 
+                                                rows="1"
+                                                value={responsibility} 
+                                                onChange={(e) => {handleResponsibilityChange(id, id2, e)}}
+                                            ></textarea>
+                                        </div>
+                                    ))}
+                                </div>
+                            </div>
+                        )
+                    })}
                 </div>
             </div>
         </div>
